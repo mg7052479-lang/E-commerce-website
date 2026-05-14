3448c2c4ddc090fdbaa7bf8df0b5c1276aa49089
Mohammed Ghous <mg7052479@gmail.com>
2026-05-14 22:40:10 +0530

    fix the bug and added category section

--- src/components/category.jsx
+++ src/components/category.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
 
-const category = () => {
+const Category = () => {
   return (
-    <div>
+    <div className='flex justify-start mt-6 ml-6'>
+
+      <div className='w-72 h-24 bg-gray-200 shadow-md rounded-xl flex items-center px-4 gap-6'>
+
+        {/* Image */}
+        <img
+          src="/assets/electronics/boAt Wave Beat 4.webp"
+          alt="watch"
+          className='w-16 h-16 cursor-pointer object-contain'
+        />
+
+        {/* Text */}
         <div>
-            
+          <p className='text-sm text-2xl text-gray-500'>
+            Smart Watches
+          </p>
         </div>
-      
+
+      </div>
+
     </div>
   )
 }
 
-export default category
+export default Category
