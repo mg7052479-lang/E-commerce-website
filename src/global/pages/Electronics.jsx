45c07aa72258511604f669d6b1374214d7354e11
Mohammed Ghous <mg7052479@gmail.com>
2026-06-04 13:05:28 +0530

    complete the Electronic section with the filtering feature

--- src/global/pages/Electronics.jsx
+++ src/global/pages/Electronics.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 import Layout from "../../components/Layout";
+import { products } from "../../data_json.js";
+import { useState } from "react";
 
 const Electronics = () => {
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  const [selectedPrices, setSelectedPrices] = useState([]);
+
+  const ElectronicsProducts = products.filter(
+    (product) => product.Type === "Electronics",
+  );
+
+    const handleFilter = (category) => {
+    setSelectedFilters((prev) =>
+      prev.includes(category)
+        ? prev.filter((item) => item !== category)
+        : [...prev, category]
+    );
+  };
+  const handlePriceFilter = (range) => {
+  setSelectedPrices((prev) =>
+    prev.includes(range)
+      ? prev.filter((item) => item !== range)
+      : [...prev, range]
+  );
+};
+const filteredProducts = ElectronicsProducts.filter((product) => {
+  const categoryMatch =
+    selectedFilters.length === 0 ||
+    selectedFilters.includes(product.Category);
+
+  const priceMatch =
+    selectedPrices.length === 0 ||
+    selectedPrices.some((range) => {
+      const [min, max] = range.split("-").map(Number);
+
+      return (
+        product.Price >= min &&
+        product.Price <= max
+      );
+    });
+
+  return categoryMatch && priceMatch;
+});
+
   return (
     <Layout>
       <div className="mt-20 flex gap-6 p-4">
@@ -12,22 +54,31 @@ const Electronics = () => {
 
           <div className="flex flex-col gap-4 mb-4 p-2 mt-3">
             <label>
-              <input type="checkbox" />
+              <input type="checkbox"
+              onChange={() => handleFilter("Airdops")}
+    
+              />
               Airdops
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox"
+              onChange={() => handleFilter("Headphones")}
+              />
               Headphones
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox"
+              onChange={() => handleFilter("Watch")}
+               />
               watches
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox"
+              onChange={() => handleFilter("Mobile")}
+               />
               Mobile
             </label>
           </div>
@@ -38,54 +89,60 @@ const Electronics = () => {
 
           <div className="flex flex-col gap-4 p-2 mt-3">
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" 
+              onChange={() => handlePriceFilter("1000-2000")}
+              />
               ₹1000 - 2000
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" 
+              onChange={() => handlePriceFilter("2000-5000")}
+              />
               ₹2000 - ₹5000
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" 
+              onChange={() => handlePriceFilter("5000-10000")}
+              />
               ₹5000 - ₹10000
             </label>
 
             <label>
-              <input type="checkbox" />
+              <input type="checkbox" 
+              onChange={() => handlePriceFilter("10000-20000")}
+              />
               ₹10000 - ₹20000
             </label>
           </div>
         </div>
-         
-         <div className="flex flex-wrap mt-18 justify-evenly gap-6 flex-1">
-            <div
-              className="w-72 bg-white rounded-xl shadow-md hover:scale-105 transition-all duration-300 p-4"
-            >
-              <img
-                src= "/assets/electronics/boAt Rockerz 558 3.webp"
-                alt="product"
-                className="w-full h-72 object-contain"
-              />
 
-              <h3 className="font-bold mt-2">
-                boAt Rockerz 558
-              </h3>
+        <div className="flex flex-wrap justify-evenly gap-6  flex-1">
+          {filteredProducts.map((product, index) => {
+            return (
+              <div 
+              key={index}
+               className="w-72 bg-white  rounded-xl shadow  hover:scale-105 transition-all duration-300 p-4">
+                <img
+                  src={product.Image[0]}
+                  alt={product.Name}
+                  className="w-full h-72 object-contain"
+                />
 
-          <div className="flex items-center gap-2">
-              <p className="line-through font-bold">
-                ₹{2000}
-              </p>
-              <p className="text-lg text-green-600  ">
-                ₹{1500}
-              </p>
-            </div>  
-          </div>
-</div>
-  </div>
+                <h3 className="font-bold mt-2">{product.Name}</h3>
+
+                <div className="flex items-center gap-2">
+                  <p className="line-through font-bold">₹{product.Price+500}</p>
+                  <p className="text-lg text-green-600  ">₹{product.Price}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </Layout>
-);
+  );
 };
 
 export default Electronics;
